b8a63c0d9b241ccc6f2bb553046cd58b60827ba7
kohjun <kohjun@gmail.com>
2024-11-18 16:57:50 +0900

    11th_programming

--- models/Event.js
+++ models/Event.js
@@ -10,7 +10,7 @@ const eventSchema = new mongoose.Schema({
   endTime: String,
   participation_fee: Number,
   contents: String,
-  creator: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  creator: mongoose.Schema.Types.ObjectId,
   isEnded: { type: Boolean, default: false },
   finalParticipants: { type: [String], default: [] }, // 참가자들의 ID 저장
 });
