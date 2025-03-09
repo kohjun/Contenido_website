39caf5d11b6edd4c4dcbb9d3204e9bae7529bbe5
kohjun <kohjun@gmail.com>
2025-03-09 22:19:11 +0900

    +

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(express.json());
 
 // CORS 설정 (필요한 경우)
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');  // 웹사이트 주소 입력
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
