a097bb484b8747f03b98c26dd2cec3858bff3d42
kohjun <kohjun1234@gmail.com>
2025-04-08 22:12:30 +0900

    작업 중인 mypage.js 변경사항 커밋

--- public/js/mypage.js
+++ public/js/mypage.js
@@ -1,12 +1,16 @@
 // 사용자 정보 표시
 
 let userData; // 사용자 데이터를 전역 변수로 저장
-
+// 성별 표시 매핑
+    const genderDisplay = {
+      'male': '남성',
+      'female': '여성',
+      'other': '기타'
+    };
 // mypage.js를 수정
 async function fetchUserInfo() {
   try {
     const response = await fetch('/user/info_database');
-    
     if (response.status === 401) {
       // 로그인되지 않은 경우 현재 URL을 state 파라미터로 전달하여 카카오 로그인으로 리디렉션
       const currentUrl = encodeURIComponent(window.location.href);
@@ -63,12 +67,6 @@ async function fetchUserInfo() {
       'starterTeam' :'스타터팀'
     };
 
-    // 성별 표시 매핑
-    const genderDisplay = {
-      'male': '남성',
-      'female': '여성',
-      'other': '기타'
-    };
 
     // 기본 정보 업데이트
     updateElement('user-name', `이름 : ${data.name || '-'}`);
@@ -151,11 +149,9 @@ function showEditForm(id, currentValue) {
   const container = document.getElementById(id);
   const editForm = container.querySelector('.edit-form');
   const input = document.getElementById(`${id}-input`);
-  
   if (id === 'user-preferred-activity' && currentValue !== '-') {
     input.value = currentValue;
   }
-  
   editForm.style.display = 'block';
   container.querySelector('.edit-button').style.display = 'none';
 }
