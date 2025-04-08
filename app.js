28d8c87a9cbddcbf2f2bb29628fc7cd905d29376
kohjun <kohjun@gmail.com>
2025-04-08 18:51:12 +0900

    .

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use((req, res, next) => {
         res.header('Access-Control-Allow-Origin', origin);
         res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
         res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
-        res.header('Access-Control-Allow-Credentials', 'true');
+        res.header('Access-Control-Allgiow-Credentials', 'true');
     }
     if (req.method === 'OPTIONS') {
         return res.sendStatus(200);
